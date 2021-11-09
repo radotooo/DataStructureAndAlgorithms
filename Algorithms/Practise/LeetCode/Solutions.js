63a6fe47738ee1643da84d420d190d32992e182d
radotooo <radotooo@gmail.com>
2021-11-09 22:14:52 +0200

    practice

--- Algorithms/Practise/LeetCode/Solutions.js
+++ Algorithms/Practise/LeetCode/Solutions.js
@@ -7,9 +7,9 @@
     let result = 0
      for (let i = 0; i < nums.length; i++) {
         for (let j = i; j < nums.length; j++) {
-            
+
             if (nums[j] === nums[i] && j > i) result++;
-        } 
+        }
      }
     return result
 };
@@ -33,7 +33,7 @@
 function binarySearch(nums, target, left, right) {
 if (left > right) {
     return nums.length
-}    
+}
 
 const mid = Math.floor((left + right) / 2)
     if (nums[mid] === target) {
@@ -59,9 +59,9 @@ const mid = Math.floor((left + right) / 2)
      for (let index = 0; index < numRows -1; index++) {
          const currentArr = result[index]
 
-         const temp = currentArr.reduce((a,b,i) => { 
+         const temp = currentArr.reduce((a,b,i) => {
             if (i === currentArr.length - 1) return a
-            
+
             a.push(currentArr[i] + currentArr[i + 1])
 
             return a
@@ -75,7 +75,56 @@ const mid = Math.floor((left + right) / 2)
      return result
 };
 
+class TreeNode {
+    constructor(val, left, right) {
+        this.val = val
+        this.left = left
+        this.right = right
+    }
+}
 
-console.log(generate(6))
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+ var diameterOfBinaryTree = function(root) {
+    let result = 0;
+    helper(root);
+    return result
 
+    function helper(root){
+        if(!root)return 0;
+
+        let left = helper(root.left);
+        console.log(left);
+        let right = helper(root.right);
+
+        console.log(right);
+        result = Math.max(left + right , result);
+
+        return 1 + Math.max(left, right)
+    }
+};
+
+// 231. Power of Two
+var isPowerOfTwo = function(n) {
+    if (n === 1) return true
+    if (n < 1) return false
+    return isPowerOfTwo(n/2)
+};
+
+// Alternative 1
+var isPowerOfTwo = function (n) {
+    return  Math.log2(n) % 1 === 0;
+    };
+
+console.log(isPowerOfTwo(3));
 // console.log(numIdenticalPaires([1,2,3,1,1,3]))
